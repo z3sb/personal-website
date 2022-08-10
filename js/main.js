45b4d96712152410e9350fcab51b1c8205d43a92
Haider Fadel <oslvuao@gmail.com>
2022-08-10 23:05:09 +0300

    Add My Accounts

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-/* Typing Animation */
+/* Typing Animation Start */
 var typed = new Typed('.typing', {
   strings:['', 'Programmer', 'Web Developer', 'Web Designer'],
   typeSpeed:100,
   BackSpeed:60,
   loop:true
-})
+});
+/* Typing Animation End */
+/* Aside Start */
+const nav = document.querySelector('.nav'),
+      navList = document.querySelectorAll('li'),
+      totalNavList = navList.length;
+
+for (let i = 0 ; i < totalNavList; i++){
+  const a = navList[i];
+  a.addEventListener('click', () => {
+    this.classList.add('active')
+  })
+}
+
+/* Aside End */
