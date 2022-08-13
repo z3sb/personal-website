caac59d35a8c493b802af3fac1bbbe022ce068f6
Haider Fadel <oslvuao@gmail.com>
2022-08-13 21:12:48 +0300

    Keep Themes in LocalStorage

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -1,3 +1,7 @@
+window.addEventListener('load', () => {
+  document.body.classList.add(localStorage.getItem('color'));
+  document.body.classList.add(localStorage.getItem('theme'));
+});
 const styleSwitcherToggler = document.querySelector('.style-switcher-toggler');
 const switcher = document.querySelector('.style-switcher');
 // Open And Close Style Switcher
@@ -10,20 +14,31 @@ window.addEventListener('scroll', () => {
   }
 })
 // Change Color
-const colorSwitcher = document.getElementById('color-switcher');
+const body = document.body;
 const spans = document.querySelectorAll('.style-switcher .colors span');
-spans.forEach((e) => {
-  e.addEventListener('click', () => {
-    colorSwitcher.href = `css/skins/${e.className}.css`;
+for(let i = 1 ;  i <= spans.length ; i++){
+  spans[i - 1].addEventListener('click', () => {
+    let classes = body.className.split(' ');
+    classes.forEach(e => {
+      if (e[0] == `c`){
+        body.classList.remove(e);
+      }
+      localStorage.setItem('color', spans[i - 1].className);
+      body.classList.add(localStorage.getItem('color'));
+    })
   })
-});
-
+}
 // Them Dark and Light Mode
 let dayNight = document.querySelector('.day-night');
 dayNight.addEventListener('click', () => {
   dayNight.querySelector('i').classList.toggle('fa-sun');
   dayNight.querySelector('i').classList.toggle('fa-moon');
-  document.body.classList.toggle('dark')
+  document.body.classList.toggle('dark');
+  if(localStorage.getItem('theme') === 'light'){
+    localStorage.setItem('theme', 'dark')
+  } else{
+    localStorage.setItem('theme', 'light')
+  }
 })
 window.addEventListener('load', () => {
   if (document.body.classList.contains('dark')){
